eee8f33b18830cacf4de773c31eae780e191c363
Sergey Belyakov <sabelyakov@edu.hse.ru>
2024-01-05 02:46:07 +0300

    Fix default ctor issue

--- app/main.cpp
+++ app/main.cpp
@@ -2,7 +2,9 @@
 #include "example_frame.h"
 
 int main() {
-    REngine::Driver::Promote(std::make_unique<REngine::SingleFrameDriver>(std::make_unique<ExampleFrame>()));
+    REngine::Driver::Promote(std::make_unique<REngine::SingleFrameDriver>(
+            std::make_unique<ExampleFrame>(),
+            REngine::SingleFrameDriver::Settings{ .minimumUpdateDelayMs = 0 } ));
     REngine::Driver::King()->Initialize();
     REngine::Driver::King()->Run();
 }
